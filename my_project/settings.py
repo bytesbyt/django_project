7929b0222793546cfbea5ac353947065204fd72e
Tes R <tesryu39@gmail.com>
2024-01-28 09:57:49 +0000

    update setting

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x%5_@hmn^!8%iw!k5g9p1wwi4io4o(wiqr54s=c32nj3nm&qgw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
